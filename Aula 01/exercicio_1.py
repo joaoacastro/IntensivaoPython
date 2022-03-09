ef36ffe99449a9d2558cb5bfce4d5f9f642e928b
João Antônio de Castro <joaoaccastro@gmail.com>
2022-03-09 11:30:15 -0300

    Add files via upload

---
+++ Aula 01/exercicio_1.py
@@ -0,0 +1,62 @@
+import pyautogui
+import pyperclip
+import time
+import pandas as pd
+
+pyautogui.PAUSE = 1 #pausa de 1 segundo entre cada comando
+
+pyautogui.press('win')
+pyautogui.write('chrome')
+pyautogui.press('enter')
+time.sleep(2)
+pyautogui.hotkey('alt','space','x')
+pyautogui.hotkey('ctrl','t')
+pyperclip.copy('https://drive.google.com/drive/folders/149xknr9JvrlEnhNWO49zPcw0PW5icxga')
+pyautogui.hotkey('ctrl', 'v')
+pyautogui.press('enter')
+
+time.sleep(3) #depois do comando anterior ele espera 3 segundos
+
+pyautogui.click(x=419, y=257, clicks=2)
+
+time.sleep(3)
+
+pyautogui.click(x=440, y=317)
+pyautogui.click(x=1163, y=161)
+pyautogui.click(x=1006, y=579)
+
+time.sleep(4)
+
+tabela = pd.read_excel(r'C:\Users\1801 Desktop\Downloads\Vendas - Dez.xlsx')
+
+faturamento = tabela['Valor Final'].sum()
+quantidade = tabela['Quantidade'].sum()
+
+pyautogui.hotkey('ctrl','t')
+pyperclip.copy('https://mail.google.com/')
+pyautogui.hotkey('ctrl', 'v')
+pyautogui.press('enter')
+time.sleep(5)
+
+pyautogui.click(x=112, y=181)
+time.sleep(2)
+
+pyautogui.write('maaminaya@gmail.com')
+pyautogui.press('tab')
+pyautogui.press('tab')
+pyautogui.write('amu tu')
+pyautogui.press('tab')
+texto = f'''
+        Te Amu muito meu amol,
+        Obligado pul sempli estal du meu lado!!
+        Bola pla cima que é tudo nosso!! ♥
+        obs.: ignola essa palte de baixu hihi
+
+        Exercício de Python:
+        O faturamento de ontem foi de: R$ {faturamento:,.2f}
+        A quantidade de produtos foi de: {quantidade}
+'''
+pyperclip.copy(texto)
+pyautogui.hotkey('ctrl','v')
+
+pyautogui.hotkey('ctrl','enter')
---
+++ Aula 01/mousePosition.py
@@ -0,0 +1,5 @@
+import pyautogui
+import time
+
+time.sleep(4) #pausa o cod por 5 segundos
+print(pyautogui.position()), #esse codigo fala a posição atual do mouse
